3d8084f0ceaac3958576484119fca9ec00c1d9a7
Samuelkb <samuelk.b.hc@gmail.com>
2019-07-25 21:03:29 -0500

    implementar jwt

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,8 +1,18 @@
-const signup = () => {
-    return {
-        message: "Todo chido",
-        token: "abc.def.ghj"
-    }
+const jwt = require('jsonwebtoken');
+
+const signup = (email, password) => {
+    const payload = {
+        email,
+        password,
+    };
+
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, 'paqkieresabereso', (err, token) => {
+            err
+            ? reject({message: "Hubo un error...", token: err })
+            : resolve({message: "Token generado", token});
+        });
+    });
 }
 
 module.exports = {

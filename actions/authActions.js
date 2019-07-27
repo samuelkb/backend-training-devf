1c58152363c9e3299e8a43b454bc7a6a261a5a90
Samuelkb <samuelk.b.hc@gmail.com>
2019-07-26 20:25:03 -0500

    Generar usuario y devolver token

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,18 +1,31 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const signup = (email, password) => {
-    const payload = {
+
+    const newUser = new User({
         email,
         password,
+    });
+
+    let payload = {
+        email: null,
+        password: null,
     };
 
-    return new Promise((resolve, reject) => {
-        jwt.sign(payload, 'paqkieresabereso', (err, token) => {
-            err
-            ? reject({message: "Hubo un error...", token: err })
-            : resolve({message: "Token generado", token});
-        });
-    });
+    return newUser.save()
+            .then(user => {
+                return new Promise((resolve, reject) => {
+                    jwt.sign({email: user.email, password: user.password}, 'paqkieresabereso', (err, token) => {
+                        err
+                        ? reject({message: "Hubo un error...", token: err })
+                        : resolve({message: "Token generado", token});
+                    });
+                });                
+            })
+            .catch(err => {
+                return Promise.reject({message: 'Error', token: err})
+            });
 }
 
 module.exports = {

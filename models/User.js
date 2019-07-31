d6d95c97cb76b922428726351d89ae73b7088e3a
Samuelkb <samuelk.b.hc@gmail.com>
2019-07-30 19:50:50 -0500

    Se añade hash de contraseña con bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema({
     first_name: {
@@ -27,6 +29,27 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: true,
     }
+});
+
+UserSchema.pre('save', function(next){
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
